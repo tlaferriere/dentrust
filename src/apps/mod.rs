57d151e3e9011c4b7e579dbd83bb700440187b05
Thomas Laferriere <thomas.laferriere@bell.ca>
2022-09-21 09:55:12 -0400

    First ui with tabs

---
+++ src/apps/mod.rs
@@ -0,0 +1,9 @@
+mod agenda;
+mod billing;
+mod management;
+mod patient_files;
+
+pub use agenda::Agenda;
+pub use billing::Billing;
+pub use management::Management;
+pub use patient_files::PatientFiles;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use eframe::{HardwareAcceleration, Theme};
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        always_on_top: false,
+        maximized: false,
+        decorated: true,
+        fullscreen: false,
+        drag_and_drop_support: true,
+        icon_data: None,
+        initial_window_pos: None,
+        initial_window_size: None,
+        min_window_size: None,
+        max_window_size: None,
+        resizable: true,
+        transparent: true,
+        vsync: true,
+        multisampling: 0,
+        depth_buffer: 0,
+        stencil_buffer: 0,
+        hardware_acceleration: HardwareAcceleration::Required,
+        renderer: Default::default(),
+        follow_system_theme: true,
+        default_theme: Theme::Light,
+        run_and_return: false,
+    };
     eframe::run_native(
-        "eframe template",
+        "DenTrust",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(dentrust::DentrustApp::new(cc))),
     );
 }
 
